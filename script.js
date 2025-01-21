ef1ae0b4d1e1faa830286c649d51a8204d14768a
SSANDBY <73397626+SSANDBY@users.noreply.github.com>
2025-01-21 15:50:58 +0300

    fixed the player's answer to lower case to match the computer

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 let answer = prompt("Rock, Paper or Scissors?","");
+answer = answer.toLowerCase();
 let computerChoice = Math.ceil(Math.random()*3);
-console.log(computerChoice);
+if (computerChoice == 1) {
+    computerChoice = "rock";
+    }
+else if (computerChoice == 2) {
+    computerChoice = "paper";
+    }
+else { computerChoice = "scissors";
+    }
+alert("Computer: " + computerChoice);
+if (answer == computerChoice) {
+    alert("It's a tie!");
+    }
+else if (answer == "rock") {
+    if (computerChoice == "scissors") {
+        alert("You win!");
+        }
+    else {
+        alert("You lose!");
+        }
+    }
+else if (answer == "paper") {
+    if (computerChoice == "rock") {
+        alert("You win!");
+        }
+    else {
+        alert("You lose!");
+        }
+    }
+else if(answer == "scissors") {
+    if (computerChoice == "paper") {
+        alert("You win!");
+        }
+    else {
+        alert("You lose!");
+        }
+    }
+
+
