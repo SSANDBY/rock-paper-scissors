07a8cd6dc8af96cd90c4d6ee94fe26293703e4f4
SSANDBY <73397626+SSANDBY@users.noreply.github.com>
2025-01-23 15:44:39 +0300

    added functions to get human and computer choices

--- script.js
+++ script.js
@@ -1,41 +1,10 @@
-let answer = prompt("Rock, Paper or Scissors?","");
-answer = answer.toLowerCase();
-let computerChoice = Math.ceil(Math.random()*3);
-if (computerChoice == 1) {
-    computerChoice = "rock";
-    }
-else if (computerChoice == 2) {
-    computerChoice = "paper";
-    }
-else { computerChoice = "scissors";
-    }
-alert("Computer: " + computerChoice);
-if (answer == computerChoice) {
-    alert("It's a tie!");
-    }
-else if (answer == "rock") {
-    if (computerChoice == "scissors") {
-        alert("You win!");
-        }
-    else {
-        alert("You lose!");
-        }
-    }
-else if (answer == "paper") {
-    if (computerChoice == "rock") {
-        alert("You win!");
-        }
-    else {
-        alert("You lose!");
-        }
-    }
-else if(answer == "scissors") {
-    if (computerChoice == "paper") {
-        alert("You win!");
-        }
-    else {
-        alert("You lose!");
-        }
-    }
-
-
+function getComputerChoice(){
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+function getHumanChoice (){
+    let humanChoice = prompt('Enter your choice: rock, paper or scissors');
+    return humanChoice;
+}
+console.log(getHumanChoice());
