63e597e8e5a93a003d927412d2bfd5ad67036250
SSANDBY <73397626+SSANDBY@users.noreply.github.com>
2025-01-23 15:56:29 +0300

    added a playRound function for playing a round

--- script.js
+++ script.js
@@ -7,4 +7,38 @@ function getHumanChoice (){
     let humanChoice = prompt('Enter your choice: rock, paper or scissors');
     return humanChoice;
 }
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+function playRound (humanChoice, computerChoice){
+ humanChoice = humanChoice.toLowerCase();
+ if (humanChoice === computerChoice){
+     return 'It is a tie!';
+ }
+ if (humanChoice === 'rock'){
+     if (computerChoice === 'paper'){
+         computerScore++;
+         return 'Computer wins!, paper beats rock';
+     } else {
+         humanScore++;
+         return 'You win!, rock beats scissors';
+     }
+ }
+    if (humanChoice === 'paper'){
+        if (computerChoice === 'scissors'){
+            computerScore++;
+            return 'Computer wins!, scissors beats paper';
+        } else {
+            humanScore++;
+            return 'You win!, paper beats rock';
+        }
+    }
+    if (humanChoice === 'scissors'){
+        if (computerChoice === 'rock'){
+            computerScore++;
+            return 'Computer wins!, rock beats scissors';
+        } else {
+            humanScore++;
+            return 'You win!, scissors beats paper';
+        }
+    }
+}
